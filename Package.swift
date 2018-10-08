cb93f0005b59090e389d7befb2718ca7e30557c2
Kristaps Grinbergs <fassko@gmail.com>
2018-10-08 14:48:21 +0300

    Update to Swift 4.2

--- Package.swift
+++ Package.swift
@@ -1,11 +1,10 @@
-// swift-tools-version:4.1
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
     name: "QminderAPIMock",
     dependencies: [
-        // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.1.0"),
     ],
     targets: [
         .target(name: "App", dependencies: ["Vapor"]),
