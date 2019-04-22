f5b0cc99cc98a7469b98dfa3fc4034f5dab2eaec
Kristaps Grinbergs <fassko@gmail.com>
2019-04-22 14:37:43 +0300

    Update vapor

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:4.1
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
     name: "QminderAPIMock",
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.1.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.3.0"),
     ],
     targets: [
         .target(name: "App", dependencies: ["Vapor"]),
